5bd1973321ff7d2486c9f2f5a1d7f764516b9855
danyowoj <latsuk1803@gmail.com>
2024-05-09 13:34:33 +0700

    Minor changes

--- 8 - Golden Ratio/program.cpp
+++ 8 - Golden Ratio/program.cpp
@@ -1,30 +1,19 @@
-#include <math.h>
 #include <iostream>
-#define M_E 2.71828182845904523536
+#include <math.h>
 
 using namespace std;
 
 double f(double x)
 {
     return -(x * x * x) + 9 * (x * x) - 24 * x - 2;
-} // Здесь ваша формула
-
-//---------------------------------------------------------------------------
-double ab(double i)
-{
-    if (i < 0)
-        return -i;
-    else
-        return i;
-} // это модуль
-
-//---------------------------------------------------------------------------
+}
 
 int main()
 {
     double a, b, g, h, fu1, fu2, eps = 0.000001;
 
-    for (int i = -1; i <= 1; i += 2) // Сначало ищем минимум, затем максимум
+    // at first we calculate min, then we calculate max
+    for (int i = -1; i <= 1; i += 2)
     {
 
         a = -5;
@@ -43,7 +32,10 @@ int main()
                 b = h;
                 h = g;
                 fu2 = fu1;
-                g = a + (b - a) * (0.38); // Основная формула
+
+                // main formula
+                g = a + (b - a) * (0.38);
+
                 fu1 = i * f(g);
             }
             else
@@ -51,10 +43,13 @@ int main()
                 a = g;
                 g = h;
                 fu1 = fu2;
-                h = b - 0.38 * (b - a); // Основная формула
+
+                // main formula
+                h = b - 0.38 * (b - a);
+
                 fu2 = i * f(h);
             }
-        } while (ab(a - b) > eps); // Условие выхода
+        } while (fabs(a - b) > eps);
         i == -1 ? cout << "Maximum " : cout << "Minimum ";
         cout << "= " << (fu1 - fu2) / 2 + fu2 << "; x = " << h << endl;
     }
