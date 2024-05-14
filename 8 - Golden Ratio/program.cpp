9c3934ce1d9dbfc94ae6cf5935fe28051fc19ec3
danyowoj <latsuk1803@gmail.com>
2024-05-14 18:36:36 +0700

    Fixed major bug

--- 8 - Golden Ratio/program.cpp
+++ 8 - Golden Ratio/program.cpp
@@ -1,23 +1,22 @@
-#include <iostream>
 #include <math.h>
+#include <iostream>
 
-using namespace std;
-
+// our function
 double f(double x)
 {
-    return -(x * x * x) + 9 * (x * x) - 24 * x - 2;
+    return x * x - 6 * x;
 }
 
 int main()
 {
-    double a, b, g, h, fu1, fu2, eps = 0.000001;
+    double a, b, g, h, fu1, fu2, eps = 0.00000001;
 
-    // at first we calculate min, then we calculate max
+    // at first we search for Min, then for Max
     for (int i = -1; i <= 1; i += 2)
     {
 
-        a = -5;
-        b = 3;
+        a = -10;
+        b = 10;
 
         g = a + (b - a) * (0.38);
         h = b - 0.38 * (b - a);
@@ -29,29 +28,26 @@ int main()
 
             if (fu1 <= fu2)
             {
+                // main formula
                 b = h;
                 h = g;
                 fu2 = fu1;
-
-                // main formula
                 g = a + (b - a) * (0.38);
-
                 fu1 = i * f(g);
             }
             else
             {
+                // main formula
                 a = g;
                 g = h;
                 fu1 = fu2;
-
-                // main formula
                 h = b - 0.38 * (b - a);
-
                 fu2 = i * f(h);
             }
-        } while (fabs(a - b) > eps);
-        i == -1 ? cout << "Maximum " : cout << "Minimum ";
-        cout << "= " << (fu1 - fu2) / 2 + fu2 << "; x = " << h << endl;
+        } while (fabs(a - b) > eps); // exit condition
+
+        i == -1 ? std::cout << "Max: " : std::cout << "Min: ";
+        std::cout << "F(x) = " << (fu1 - fu2) / 2 + fu2 << "; \tx = " << h << std::endl;
     }
 
     return 0;
